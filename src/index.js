1422eea65e36e6e83eda48a3a1672116d75bbb2b
Raj Kasaudhan <rajksd9@gmail.com>
2024-02-12 21:12:26 +0530

    FEAT : ADDED RATE LIMITER

--- src/index.js
+++ src/index.js
@@ -1,13 +1,24 @@
 const express = require("express");
 const app = express();
+const rateLimit = require("express-rate-limit");
 const { logConfig } = require("./config");
 
 const { ServerConfig } = require("../src/config");
 const apiRoutes = require("../src/routes");
 const { infoController } = require("./controllers");
 
+// setting up ratelimiter
+
+const limiter = rateLimit({
+  windowMs: 10 * 60 * 1000, // 15 minutes
+  limit: 50, // Limit each IP to 100 requests per `window` (here, per 15 minutes).
+  standardHeaders: "draft-7",
+  legacyHeaders: false, // Disable the `X-RateLimit-*` headers.
+});
+
 app.use(express.urlencoded({ extended: true }));
-app.use(express.json())
+app.use(express.json());
+app.use(limiter);
 app.use("/api", apiRoutes);
 
 app.get("/", infoController.info);
